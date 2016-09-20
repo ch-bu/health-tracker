086fefcadb246926000fdf574ec1735b521d8674
Christian Burkhart <burkhart.christian@gmail.com>
2016-09-20 19:10:41 +0200

    listen for keydown events on searchbar

--- app/scripts/app/collections/food-collection.js
+++ app/scripts/app/collections/food-collection.js
@@ -4,7 +4,9 @@ define(['underscore', 'backbone', 'app/models/food-model'], function(_, Backbone
 
     initialize: function() {
       console.log('food collection');
-    }
+    },
+
+    url: 'https://api.nutritionix.com/v1_1/search'
   });
 
   return FoodCollection;
--- app/scripts/app/views/main.js
+++ app/scripts/app/views/main.js
@@ -3,13 +3,21 @@ define(['backbone', 'app/models/food-model',
 
   var AppView = Backbone.View.extend({
 
-    el: '.container',
+    el: 'main',
+
+    events: {
+      'keydown #food-search': 'searchFood',
+    },
 
     initialize: function() {
       console.log('initalize app');
       var model = new FoodModel();
       var foodCollection = new FoodCollection();
       console.log(foodCollection);
+    },
+
+    searchFood: function() {
+      console.log('food');
     }
 
   });
