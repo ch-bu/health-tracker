f134cf0b36c354fef879ecb4a969234a5b04436c
Christian Burkhart <burkhart.christian@gmail.com>
2016-11-06 13:30:35 +0100

    first verison of build in r.js is working

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -30,27 +30,3 @@ require.config({
     }
   }
 });
-
-
-require(['backbone', 'mainView'], function(Backbone, AppView) {
-
-  // Set myFoods storage if non-existent
-  if (localStorage.getItem('myFoods') === null) {
-    localStorage.setItem('myFoods', '[]');
-  }
-
-  // Set date storage of food if not existent
-  if (localStorage.getItem('foodTracker') === null) {
-    localStorage.setItem('foodTracker', '{}');
-  }
-
-
-  new AppView();
-
-  // Array Remove - By John Resig (MIT Licensed)
-  Array.prototype.remove = function(from, to) {
-    var rest = this.slice((to || from) + 1 || this.length);
-    this.length = from < 0 ? this.length + from : from;
-    return this.push.apply(this, rest);
-  };
-});
--- build.js
+++ build.js
@@ -1,28 +1,28 @@
 ({
-  baseUrl: '.',
-  // appDir: 'app',
-  // dir: '../../dist/scripts',
-  // mainConfigFile: 'app/scripts/app/views/main.js',
-  name: 'app/scripts/app/views/main.js',
+  baseUrl: './',
+  appDir: 'app/scripts',
+  dir: '.tmp/scripts/build',
+  mainConfigFile: 'app/scripts/app.js',
+  // name: 'app/views/main.js',
 //
-  // modules: [
-  //   {
-  //     name: 'scripts/app/views/main',
+  modules: [
+    {
+      name: 'app',
 
-  //     include: [
-  //       'jquery',
-  //       'backbone',
-  //     ]
-  //   },
+      // include: [
+      //   'vendor/jquery',
+      //   'vendor/backbone',
+      // ]
+    },
 
     // {
     //   name: 'mainview',
     //   include: ['views/main', 'models/model']
     // }
-  // ],
+  ],
 
   optimize: 'uglify2',
 
-  out: 'main-built.js'
+  // out: 'main-built.js'
 
 });
